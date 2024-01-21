53dc1d514133e90601e0318398b7194f99f54706
niranjankolpe <en20170973@git-india.edu.in>
2024-01-21 19:58:27 +0530

    Included support for Vercel

--- detector_app/views.py
+++ detector_app/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 
 # Create your views here.
-context = {'media_url': settings.MEDIA_URL}
-  
 def index(request):
     return redirect("home")
 
 def home(request):
+    context = {'media_url': settings.MEDIA_URL}
     return render(request, "detector_app/index.html", context)
