4a0942a9e3eb4891964a182e3c86f6fb5a910a1d
niranjankolpe <en20170973@git-india.edu.in>
2024-01-21 20:15:23 +0530

    Added requirements.txt

--- smh_django_project/settings.py
+++ smh_django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d--xm-qd(e)!bichs)0sng&yo+xri#jsz94vt3dx5@c#j76nc#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost',]
 
 
 # Application definition
