954b6bcafde415a51646e29f337c954c7696cd26
niranjankolpe <en20170973@git-india.edu.in>
2024-01-21 20:33:22 +0530

    Updated for accessing  model.txt

--- detector_app/views.py
+++ detector_app/views.py
@@ -6,5 +6,8 @@ def index(request):
     return redirect("home")
 
 def home(request):
-    context = {'media_url': settings.MEDIA_URL}
+    read_text = ""
+    with open("media_files/models/model.txt", "r") as model:
+        read_text = " ".join(model.readlines())
+    context = {'media_url': settings.MEDIA_URL, 'read_text': read_text}
     return render(request, "detector_app/index.html", context)
